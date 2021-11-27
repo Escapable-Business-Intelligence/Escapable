54c1213fdbc319e0007b69860fa2b2ac31d1d6f2
soobeen <b2nyb2st77@gmail.com>
2021-11-27 23:06:06 +0900

    테마 추천 UI 꾸미기, 머신러닝 연동 준비

--- escapable/cafe/urls.py
+++ escapable/cafe/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:cafe_index>/', views.detail, name='detail'),
     path('recommend/', views.recommend, name='recommend'),
+    path('recommend1/', views.recommend1, name='recommend1'),
+    path('recommend2/', views.recommend2, name='recommend2'),
     path('selectThema', views.selectThema, name='selectThema'),
+    path('recommendThema1', views.recommendThema1, name='recommendThema1'),
+    path('recommendThema2', views.recommendThema2, name='recommendThema2'),
 ]
--- escapable/cafe/views.py
+++ escapable/cafe/views.py
@@ -15,13 +15,26 @@ def detail(request, cafe_index):
     return HttpResponse("You're looking at cafe %s." % cafe_index)
 
 def recommend(request):
+
+    return render(request, 'recommend.html')
+
+def recommend1(request):
     cafe_list = Cafe.objects.all()
 
     context = {
         'cafe_list': cafe_list,
     }
 
-    return render(request, 'recommend.html', context)
+    return render(request, 'recommend1.html', context)
+
+def recommend2(request):
+    cafe_list = Cafe.objects.all()
+
+    context = {
+        'cafe_list': cafe_list,
+    }
+
+    return render(request, 'recommend2.html', context)
 
 @csrf_exempt
 def selectThema(request):
@@ -31,3 +44,29 @@ def selectThema(request):
     thema_list = list(thema_list.values())
 
     return JsonResponse(thema_list, safe=False)
+
+@csrf_exempt
+def recommendThema1(request):
+    data_list = json.loads(request.body)
+
+    print(data_list)
+
+    #머신러닝 처리 -> 추천 테마 데이터 받아오기
+
+    context = {
+        'result': data_list,
+    }
+
+    return JsonResponse(context, safe=False)
+
+@csrf_exempt
+def recommendThema2(request):
+    data_list = json.loads(request.body)
+
+    #머신러닝 처리 -> 추천 테마 데이터 받아오기
+
+    context = {
+        'result': data_list,
+    }
+
+    return JsonResponse(context, safe=False)
